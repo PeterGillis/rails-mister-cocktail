3fc231e1310f096cd69817fb722af23ba3347c5e
Peter Gillis <petergillis@hotmail.com>
2019-05-17 14:05:47 +0200

    back-end finished, front end begins

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -13,7 +13,7 @@ class DosesController < ApplicationController
 
   def destroy
     @dose = Dose.find(params[:id])
-    @dose.destroy
+    @dose.delete
     redirect_to cocktail_path(@dose.cocktail)
   end
 
